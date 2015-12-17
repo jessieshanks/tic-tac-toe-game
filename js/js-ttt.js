6a62ee2747dd5de96a356594dfceb9c5ef63e5a7
Jessie Shanks <jessie.shanks@gmail.com>
2015-12-16 21:09:33 -0600

    preliminay js

--- js/js-ttt.js
+++ js/js-ttt.js
@@ -1,64 +1,119 @@
 
-/*var gameBoard=[[0,0,0],[0,0,0],[0,0,0]];
+//Declare Global Variables
+var gameBoard;
 
 
 
-
-
-function CreateGameBoard(newBoard) {
-  for [i=0; i<=2; i++] {
-    for [j=0; j<=2; j++] {
-      newBoard [i][j] = 0;
-    }
-  }
-  return newBoard;
+function newBoard() {
+//resets game board
+  gameBoard=[[0,0,0],[0,0,0],[0,0,0]]
 }
 
-function checkWin(board, playerID) {
+function consoleLogBoard () {
+//prints game board to console log
+  for (i=0; i<=2; i++) {
+    for (j=0; j<=2; j++) {
+      console.log(gameBoard[i][j]);
+    }
+  }
+}
+
+var x=1;
+//gameBoard = [[1,1,1],[0,0,0],[0,0,0]];
+gameBoard = [[1,0,0],[1,0,0],[1,0,0]];
+//gameBoard = [[1,1,1],[0,0,0],[0,0,0]];
+
+var winTest = [
+  function() {return (gameBoard[0][0]===x) && (gameBoard[0][1]===x) && (gameBoard[0][2]===x)},
+  function() {return (gameBoard[1][0]===x) && (gameBoard[1][1]===x) && (gameBoard[1][2]===x)},
+  function() {return (gameBoard[2][0]===x) && (gameBoard[2][1]===x) && (gameBoard[2][2]===x)},
+  function() {return (gameBoard[0][0]===x) && (gameBoard[1][0]===x) && (gameBoard[2][0]===x)},
+  function() {return (gameBoard[0][1]===x) && (gameBoard[1][1]===x) && (gameBoard[2][1]===x)},
+  function() {return (gameBoard[0][2]===x) && (gameBoard[1][2]===x) && (gameBoard[2][2]===x)},
+  function() {return (gameBoard[0][0]===x) && (gameBoard[1][1]===x) && (gameBoard[2][2]===x)},
+  function() {return (gameBoard[2][0]===x) && (gameBoard[1][1]===x) && (gameBoard[0][2]===x)},
+];
+
+function findWin(num) {
+//find if number has won
+  console.log("findWin");
+  i=0;
+  win=false;
+  do {
+    win = winTest[i].call();
+    console.log("win value: ", win);
+    i +=1;
+  } while (!win);
+  return win;
+}
+
+
+console.log(findWin(x));
+
+//var winTest = {
+//  fuction ()
+//}
+
+
+//newBoard();
+consoleLogBoard();
+
+
+
+
+
+
+
+/*function checkWin(board, playerID) {
   board [0][1] = playerID;
   var win = {
     .h0 = ( (board[0][0]===playerID) && (board[0][1]===playerID) && (board[0][2]===playerID) )
     .h1 =
 
-functionHorizontalWin (board, playerID) {
-  var win;
-  for [i=0; i<=2; i++] {
-      win = (board[i][0]===playerID) && (board[i][1]===playerID) && (board[i][2]===playerID);
-      if (win) break;
-  }
-}
-
-  do {
-
-    while }
-  }
-
-while !(win)
-
-var "H" "V" "D"
 
 
 
 
-1
-2
-3
-4
-5
-6
-7
-8
-9*/
-var a = function(){ console.log("this is function: a") }
-var b = function(){ console.log("this is function: b") }
-var c = function(){ console.log("this is function: c") }
+// functionHorizontalWin (board, playerID) {
+//   var win;
+//   for [i=0; i<=2; i++] {
+//       win = (board[i][0]===playerID) && (board[i][1]===playerID) && (board[i][2]===playerID);
+//       if (win) break;
+//   }
+// }
 
-var foo = [a,b,c];
+//   do {
 
-while (foo.length){
-    foo.shift().call();
-}
+//     while }
+//   }
 
-for (i=0; i<=foo.length; i++) {
-  console.log ("Element number: ",i);
-}
+// while !(win)
+
+// var "H" "V" "D"
+
+
+
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// var a = function(){ console.log("this is function: a") }
+// var b = function(){ console.log("this is function: b") }
+// var c = function(){ console.log("this is function: c") }
+
+// var foo = [a,b,c];
+
+// while (foo.length){
+//     foo.shift().call();
+// }
+
+// for (i=0; i<=foo.length; i++) {
+//   console.log ("Element number: ",i);
+// }
+// */
