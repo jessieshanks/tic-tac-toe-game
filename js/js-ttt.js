10b15bc7b2517a399a2c75c9b396fafb4cd2d58f
Jessie Shanks <jessie.shanks@gmail.com>
2015-12-18 14:35:57 -0600

    everything seems to work

--- js/js-ttt.js
+++ js/js-ttt.js
@@ -1,24 +1,10 @@
 
 //Declare Global Variables
 var gameBoard;
-var onPlayer;
-var playerTag;
 var color = {
   player1: "#DA627D",
   player2: "#51A3A3",
-  winner: "#FFBC42",
-  cellHover: "#EDEEC0",
 };
-var winTest = [
-  function() {return (gameBoard[0][0]===onPlayer) && (gameBoard[0][1]===onPlayer) && (gameBoard[0][2]===onPlayer)},
-  function() {return (gameBoard[1][0]===onPlayer) && (gameBoard[1][1]===onPlayer) && (gameBoard[1][2]===onPlayer)},
-  function() {return (gameBoard[2][0]===onPlayer) && (gameBoard[2][1]===onPlayer) && (gameBoard[2][2]===onPlayer)},
-  function() {return (gameBoard[0][0]===onPlayer) && (gameBoard[1][0]===onPlayer) && (gameBoard[2][0]===onPlayer)},
-  function() {return (gameBoard[0][1]===onPlayer) && (gameBoard[1][1]===onPlayer) && (gameBoard[2][1]===onPlayer)},
-  function() {return (gameBoard[0][2]===onPlayer) && (gameBoard[1][2]===onPlayer) && (gameBoard[2][2]===onPlayer)},
-  function() {return (gameBoard[0][0]===onPlayer) && (gameBoard[1][1]===onPlayer) && (gameBoard[2][2]===onPlayer)},
-  function() {return (gameBoard[2][0]===onPlayer) && (gameBoard[1][1]===onPlayer) && (gameBoard[0][2]===onPlayer)},
-];
 var gameStats = {
   gamesPlayed: 0,
   player1Wins: 0,
@@ -32,88 +18,126 @@ function offPlayer() {
 }
 
 function newBoard() {
-//reset game board
+// reset game board
   gameBoard=[[0,0,0],[0,0,0],[0,0,0]];
   $(".board-cell").css("background-color","darkGray");
   $(".winner-well").css("visibility", "hidden");
   gameBoardOn();
 }
 
-function consoleLogBoard() {
-//print game board to console log
-  for (i=0; i<=2; i++) {
-    for (j=0; j<=2; j++) {
-      console.log(gameBoard[i][j]);
-    }
-  }
+function resetStats() {
+// reset game stats and player number
+  gameStats.gamesPlayed = 0;
+  gameStats.player1Wins = 0;
+  gameStats.player2Wins = 0;
+  $(".stats-well").css("visibility", "hidden");
+  onPlayer = 2;
 }
 
 function winner() {
-//find if number has won
-  console.log("findWin");
-  i=0;
+// find if current player has won
+  var winTest = [
+    function() {return (gameBoard[0][0]===onPlayer) && (gameBoard[0][1]===onPlayer) && (gameBoard[0][2]===onPlayer)},
+    function() {return (gameBoard[1][0]===onPlayer) && (gameBoard[1][1]===onPlayer) && (gameBoard[1][2]===onPlayer)},
+    function() {return (gameBoard[2][0]===onPlayer) && (gameBoard[2][1]===onPlayer) && (gameBoard[2][2]===onPlayer)},
+    function() {return (gameBoard[0][0]===onPlayer) && (gameBoard[1][0]===onPlayer) && (gameBoard[2][0]===onPlayer)},
+    function() {return (gameBoard[0][1]===onPlayer) && (gameBoard[1][1]===onPlayer) && (gameBoard[2][1]===onPlayer)},
+    function() {return (gameBoard[0][2]===onPlayer) && (gameBoard[1][2]===onPlayer) && (gameBoard[2][2]===onPlayer)},
+    function() {return (gameBoard[0][0]===onPlayer) && (gameBoard[1][1]===onPlayer) && (gameBoard[2][2]===onPlayer)},
+    function() {return (gameBoard[2][0]===onPlayer) && (gameBoard[1][1]===onPlayer) && (gameBoard[0][2]===onPlayer)},
+  ];
+  var i=0;
   win=false;
   do {
     win = winTest[i].call();
-    console.log("win value: ", win);
     i += 1;
   } while ((!win) && (i<winTest.length));
   return win;
 }
 
+function gameBoardFull() {
+// find if it's a draw - cat's game
+  var count0=0;
+  for (i=0; i<=2; i++) {
+    for (j=0; j<=2; j++) {
+      if (gameBoard[i][j]===0) { count0+=1; }
+    }
+  }
+  return (count0===0);
+}
+
 function playerColor(number) {
+// returns color number data
   var player = ("player"+number);
   return color[player];
 }
 
-function setOnPlayerHeader() {
-  headIdName = ("player"+onPlayer+"-head");
-  document.getElementById(headIdName).style.backgroundColor = playerColor(onPlayer);
-  document.getElementById(headIdName).style.color = "white";
+function setActivePlayerHeader(playerNum) {
+  var playerTag = ("player"+playerNum);
+  document.getElementById(playerTag+"-head").style.backgroundColor = color[playerTag];
+  document.getElementById(playerTag+"-head").style.color = "white";
 }
 
-function setOffPlayerHeader() {
-  headIdName = ("player"+offPlayer()+"-head");
-  document.getElementById(headIdName).style.backgroundColor = "white";
-  document.getElementById(headIdName).style.color = playerColor(offPlayer());
+function setInactivePlayerHeader(playerNum) {
+  var playerTag = ("player"+playerNum);
+  document.getElementById(playerTag+"-head").style.backgroundColor = "white";
+  document.getElementById(playerTag+"-head").style.color = color[playerTag];
 }
 
 function newTurn() {
+// new player's turn
   onPlayer=offPlayer();
-  playerTag = "player"+onPlayer;
-  setOnPlayerHeader();
-  setOffPlayerHeader();
+  setActivePlayerHeader(onPlayer);
+  setInactivePlayerHeader(offPlayer());
 }
 
 function gameBoardOff() {
+// disable clicking on game board
     $(".board-cell").attr("disabled","disabled");
 }
 
 function gameBoardOn() {
+// enable clicking on game board
     $(".board-cell").removeAttr("disabled");
 }
 
+function showStats() {
+// show game statistics
+  $(".stats-well").css("visibility", "visible");
+  $("#player1-stats").text(gameStats.player1Wins+" / "+gameStats.gamesPlayed);
+  $("#player2-stats").text(gameStats.player2Wins+" / "+gameStats.gamesPlayed);
+}
+
 function declareWinner() {
+// actions taken to congratulate winner
   winIdName = ("#player"+onPlayer+"-winner");
   $(winIdName).css("visibility", "visible");
+  $(".winner-well").text("WINNER");
   winStatsName = ("player"+onPlayer+"Wins");
   gameStats[winStatsName] += 1;
-  $(".stats-well").css("visibility", "visible");
-  //$(.stats-well)
+  showStats();
+  setInactivePlayerHeader(onPlayer);
   gameBoardOff();
 }
 
+function declareDraw() {
+// actions taken if game ends in draw
+  $(".winner-well").css("visibility", "visible");
+  $(".winner-well").text("CAT'S EYE");
+  showStats();
+  setInactivePlayerHeader(onPlayer);
+}
+
 function newGame() {
+// start new game
   gameStats.gamesPlayed += 1;
-  console.log("new Game function");
   newBoard();
-  consoleLogBoard();
   newTurn();
 }
 
 function newMatch() {
-  console.log ("new match function");
-  onPlayer=2;
+// start new match (start over from 0 games)
+  resetStats();
   newGame();
 }
 
@@ -121,27 +145,22 @@ function newMatch() {
 
 $(document).ready(function(){
 
-  onPlayer=2;
   gameBoardOff();
 
   $('.board-cell').on('click', function() {
-    console.log ($(this).val()[0],$(this).val()[1]);
-    console.log ($(this).val().charAt(0),$(this).val().charAt(1));
-    console.log("player", onPlayer);
     gameBoard[$(this).val().charAt(0)][$(this).val().charAt(1)]=onPlayer;
-    console.log ("what will I print?");
-    consoleLogBoard();
     $(this).css("background-color", playerColor(onPlayer));
     $(this).attr("disabled","disabled");
     if (winner()) {
       declareWinner();
+    } else if (gameBoardFull()) {
+      declareDraw();
     } else {
       newTurn();
     }
   })
 
   $('#start-button').on('click', function(){
-    console.log ("start button clicked");
     if (gameStats.gamesPlayed===0) {
       newMatch();
     } else {
@@ -156,58 +175,3 @@ $(document).ready(function(){
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// functionHorizontalWin (board, playerID) {
-//   var win;
-//   for [i=0; i<=2; i++] {
-//       win = (board[i][0]===playerID) && (board[i][1]===playerID) && (board[i][2]===playerID);
-//       if (win) break;
-//   }
-// }
-
-//   do {
-
-//     while }
-//   }
-
-// while !(win)
-
-// var "H" "V" "D"
-
-
-
-
-// 1
-// 2
-// 3
-// 4
-// 5
-// 6
-// 7
-// 8
-// 9
-// var a = function(){ console.log("this is function: a") }
-// var b = function(){ console.log("this is function: b") }
-// var c = function(){ console.log("this is function: c") }
-
-// var foo = [a,b,c];
-
-// while (foo.length){
-//     foo.shift().call();
-// }
-
-// for (i=0; i<=foo.length; i++) {
-//   console.log ("Element number: ",i);
-// }
-// */
